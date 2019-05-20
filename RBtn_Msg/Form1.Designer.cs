5c13350f5104a67e9dda8f0255b680b8b2eb3461
Jiyeon Choi <ccy5906@naver.com>
2019-05-20 09:36:17 +0900

    #1 폼에 그룹박스, 라디오버튼, 버튼 추가 및 속성 설정

--- RBtn_Msg/Form1.Designer.cs
+++ RBtn_Msg/Form1.Designer.cs
@@ -28,13 +28,149 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gpOption1 = new System.Windows.Forms.GroupBox();
+            this.gpOption2 = new System.Windows.Forms.GroupBox();
+            this.OK = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.gpOption1.SuspendLayout();
+            this.gpOption2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpOption1
+            // 
+            this.gpOption1.Controls.Add(this.radioButton3);
+            this.gpOption1.Controls.Add(this.radioButton2);
+            this.gpOption1.Controls.Add(this.OK);
+            this.gpOption1.Location = new System.Drawing.Point(12, 12);
+            this.gpOption1.Name = "gpOption1";
+            this.gpOption1.Size = new System.Drawing.Size(187, 164);
+            this.gpOption1.TabIndex = 0;
+            this.gpOption1.TabStop = false;
+            this.gpOption1.Text = "type";
+            // 
+            // gpOption2
+            // 
+            this.gpOption2.Controls.Add(this.radioButton4);
+            this.gpOption2.Controls.Add(this.radioButton6);
+            this.gpOption2.Controls.Add(this.radioButton5);
+            this.gpOption2.Location = new System.Drawing.Point(205, 12);
+            this.gpOption2.Name = "gpOption2";
+            this.gpOption2.Size = new System.Drawing.Size(187, 164);
+            this.gpOption2.TabIndex = 1;
+            this.gpOption2.TabStop = false;
+            this.gpOption2.Text = "Icon";
+            // 
+            // OK
+            // 
+            this.OK.AutoSize = true;
+            this.OK.Location = new System.Drawing.Point(6, 34);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(40, 16);
+            this.OK.TabIndex = 0;
+            this.OK.TabStop = true;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "OKCancel";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 78);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "YesNo";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 78);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(73, 16);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Question";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 56);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 16);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Information";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 34);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(50, 16);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Error";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(380, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 319);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gpOption2);
+            this.Controls.Add(this.gpOption1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "기메시지 박스 보";
+            this.gpOption1.ResumeLayout(false);
+            this.gpOption1.PerformLayout();
+            this.gpOption2.ResumeLayout(false);
+            this.gpOption2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gpOption1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton OK;
+        private System.Windows.Forms.GroupBox gpOption2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Button button1;
     }
 }
 
